0cb3b3b4c22204d5fed8e38f4dfa5fc9ea059d01
Kibria934 <kibriahossain725@gmai.com>
2022-09-02 16:12:59 +0600

    update api changed

--- controllers/update_user.patch.controllers.js
+++ controllers/update_user.patch.controllers.js
@@ -4,15 +4,18 @@ module.exports.update_a_user = (req, res) => {
   const id = JSON.parse(req.params.id);
   const sended_data = req.body;
   const user_props = Object.keys(users);
+  console.log(user_props);
 
   if (typeof id === "number" && user_props.includes(String(id))) {
+    const index = user_props.indexOf(String(id));
+
     users[id - 1] = {
-      _id: users[id - 1]._id,
-      name: sended_data.name || users[id - 1].name,
-      gender: sended_data.gender || users[id - 1].gender,
-      contact: sended_data.contact || users[id - 1].contact,
-      address: sended_data.address || users[id - 1].address,
-      photoUrl: sended_data.photoUrl || users[id - 1].photoUrl,
+      _id: users[index - 1]._id,
+      name: sended_data.name || users[index - 1].name,
+      gender: sended_data.gender || users[index - 1].gender,
+      contact: sended_data.contact || users[index - 1].contact,
+      address: sended_data.address || users[index - 1].address,
+      photoUrl: sended_data.photoUrl || users[index - 1].photoUrl,
     };
     res.status(200).send({
       success: true,
